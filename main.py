a3bd93e5eb4f7734a5dabec30d3c43918bbc9a8f
GMT-Mati <75912725+GMT-Mati@users.noreply.github.com>
2021-04-15 12:34:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ df['Data waluty'] = pd.to_datetime(df['Data waluty'], dayfirst=True)  # 4
 
 # klasa wyświetlająca transakcje, wypływy, wydatki w dowolnym okresie czasu
 
-class Transakcje:
+class Transakcje():
 
     def __init__(self, year, month, start_date, end_date):
         self.year = year
@@ -24,84 +24,95 @@ class Transakcje:
         self.end_date = end_date
 
     @staticmethod
-    def liczba_transakcji_rok(year):
-        if year in range(3000):
-            df['Year'] = df['Data księgowania'].apply(lambda time: time.year)
-            print(f"Rok: {year}\n{df[df['Year'] == year].count().iloc[[0, 4, 6]]}")
-
-    @staticmethod
-    def liczba_transakcji_miesięcznie(year, month):
-        if month in range(1, 13):
-            df['Month'] = df['Data księgowania'].apply(lambda time: (time.year, time.month))
-            
-            def nazwa_miesiąca(month):
-                return {1: 'Styczeń', 2: 'Luty', 3: 'Marzec', 4: 'Kwiecień', 5: 'Maj', 6: 'Czerwiec', 7: 'Lipiec',
-                        8: 'Sierpień', 9: 'Wrzesień', 10: 'Październik', 11: 'Listopad', 12: 'Grudzień'}[month]
-
-            print(f"Miesiąc: {nazwa_miesiąca(month)}\n{df[df['Month'] == (year, month)].count().iloc[[0, 4, 6]]}")
-
-    @staticmethod
-    def liczba_transakcji_okres(start_date, end_date):
-        df['Time'] = df['Data księgowania'].between(start_date, end_date, inclusive=True)
-        print(f"Okres od {start_date} do {end_date}:\n{df.loc[df['Time']].count().iloc[[0, 4, 6]]}")
-
-    @staticmethod
     def przychody_wydatki_roczne(year):
         df['Year'] = df['Data księgowania'].apply(lambda time: time.year)
-        wykaz_dochodów = df.loc[(df['Year'] == year) &
-                                (df['Kwota operacji'] > 0)][['Data księgowania', 'Kwota operacji']]
-        wykaz_wydatków = df.loc[(df['Year'] == year) &
-                                (df['Kwota operacji'] < 0)][['Data księgowania', 'Kwota operacji']]
-        suma_dochodów = round(df.loc[(df['Year'] == year) & (df['Kwota operacji'] > 0)]['Kwota operacji'].sum(), 2)
-        suma_wydatków = round(df.loc[(df['Year'] == year) & (df['Kwota operacji'] < 0)]['Kwota operacji'].sum(), 2)
-        print(f"Rok: {year}\n{wykaz_dochodów}\nSuma dochodów: {suma_dochodów} zł")
-        print(f"Rok: {year}\n{wykaz_wydatków}\nSuma wydatków: {suma_wydatków} zł")
+        wykaz_dochodow = df.loc[(df['Year'] == year) &
+                                (df['Kwota operacji'] > 0)][['Kwota operacji', 'Data księgowania', 'Kategoria']]
+        wykaz_wydatkow = df.loc[(df['Year'] == year) &
+                                (df['Kwota operacji'] < 0)][['Kwota operacji', 'Data księgowania', 'Kategoria']]
+        suma_dochodow = round(df.loc[(df['Year'] == year) & (df['Kwota operacji'] > 0)]['Kwota operacji'].sum(), 2)
+        suma_wydatkow = round(df.loc[(df['Year'] == year) & (df['Kwota operacji'] < 0)]['Kwota operacji'].sum(), 2)
+        ilosc_transakcji_przychodu = len(df.loc[(df['Year'] == year) & (df['Kwota operacji'] > 0)]['Kwota operacji'])
+        ilosc_transakcji_wydatkow = len(df.loc[(df['Year'] == year) & (df['Kwota operacji'] < 0)]['Kwota operacji'])
+        zysk = round(suma_dochodow + suma_wydatkow, 2)
+        print(f"W roku: {year} przeprowadzono {df[df['Year'] == year].count().iloc[0]} transakcje.\n"
+              f"Suma dochodów: {suma_dochodow} zł ({ilosc_transakcji_przychodu} transakcje)"
+              f"\nSuma wydatków: {suma_wydatkow} zł ({ilosc_transakcji_wydatkow} transakcje)")
+        if zysk > 0:
+            print(f"Zysk wyniósł: {zysk} zł\n")
+        else:
+            print(f"Strata wyniosła: {zysk} zł\n")
+        print(f"Dochody w roku {year} to:\n{wykaz_dochodow}\n")
+        print(f"Wydatki w roku {year} to:\n{wykaz_wydatkow}\n")
 
     @staticmethod
     def przychod_wydatki_miesięczne(year, month):
-        if month in range(1, 13):
-            df['Month'] = df['Data księgowania'].apply(lambda time: (time.year, time.month))
+        df['Month'] = df['Data księgowania'].apply(lambda time: (time.year, time.month))
 
-            def nazwa_miesiąca(month):
-                return {1: 'Styczeń', 2: 'Luty', 3: 'Marzec', 4: 'Kwiecień', 5: 'Maj', 6: 'Czerwiec', 7: 'Lipiec',
-                        8: 'Sierpień', 9: 'Wrzesień', 10: 'Październik', 11: 'Listopad', 12: 'Grudzień'}[month]
+        def nazwa_miesiąca(month):
+            return {1: 'Styczeń', 2: 'Luty', 3: 'Marzec', 4: 'Kwiecień', 5: 'Maj', 6: 'Czerwiec', 7: 'Lipiec',
+                    8: 'Sierpień', 9: 'Wrzesień', 10: 'Październik', 11: 'Listopad', 12: 'Grudzień'}[month]
 
-            wykaz_dochodów = df.loc[(df['Month'] == (year, month)) & (df['Kwota operacji'] > 0)][
-                ['Data księgowania', 'Kwota operacji']]
-            wykaz_wydatków = df.loc[(df['Month'] == (year, month)) & (df['Kwota operacji'] < 0)][
-                ['Data księgowania', 'Kwota operacji']]
-            suma_dochodów = round(df.loc[(df['Month'] == (year, month)) &
-                                         (df['Kwota operacji'] > 0)]['Kwota operacji'].sum(), 2)
-            suma_wydatków = round(df.loc[(df['Month'] == (year, month)) &
-                                         (df['Kwota operacji'] < 0)]['Kwota operacji'].sum(), 2)
-            print(f"Miesiąc: {nazwa_miesiąca(month)}\n{wykaz_dochodów}\nSuma dochodów: {suma_dochodów} zł")
-            print(f"Miesiąc: {nazwa_miesiąca(month)}\n{wykaz_wydatków}\nSuma wydatków: {suma_wydatków} zł")
+        wykaz_dochodow = df.loc[(df['Month'] == (year, month)) & (df['Kwota operacji'] > 0)][
+            ['Kwota operacji', 'Data księgowania', 'Kategoria']]
+        wykaz_wydatkow = df.loc[(df['Month'] == (year, month)) & (df['Kwota operacji'] < 0)][
+            ['Kwota operacji', 'Data księgowania', 'Kategoria']]
+        suma_dochodow = round(df.loc[(df['Month'] == (year, month)) &
+                                     (df['Kwota operacji'] > 0)]['Kwota operacji'].sum(), 2)
+        suma_wydatkow = round(df.loc[(df['Month'] == (year, month)) &
+                                     (df['Kwota operacji'] < 0)]['Kwota operacji'].sum(), 2)
+        ilosc_transakcji_przychodu = len(df.loc[(df['Month'] == (year, month)) &
+                                                (df['Kwota operacji'] > 0)]['Kwota operacji'])
+        ilosc_transakcji_wydatkow = len(df.loc[(df['Month'] == (year, month)) &
+                                               (df['Kwota operacji'] < 0)]['Kwota operacji'])
+        zysk = round(suma_dochodow + suma_wydatkow, 2)
+        print(f"W miesiącu {nazwa_miesiąca(month)} {year} przeprowadzono: "
+              f"{df[df['Month'] == (year, month)].count().iloc[0]} transakcje.\n"
+              f"Suma dochodów: {suma_dochodow} zł ({ilosc_transakcji_przychodu} transakcje)"
+              f"\nSuma wydatków: {suma_wydatkow} zł ({ilosc_transakcji_wydatkow} transakcje)")
+        if zysk > 0:
+            print(f"Zysk wyniósł: {zysk} zł\n")
+        else:
+            print(f"Strata wyniosła: {zysk} zł\n")
+        print(f"Dochody w wybranym miesiącu to:\n{wykaz_dochodow}\n")
+        print(f"Wydatki w wybranym miesiącu to:\n{wykaz_wydatkow}\n")
             
     @staticmethod
     def przychód_wydatki_okres(start_date, end_date):
         df['Time'] = df['Data księgowania'].between(start_date, end_date, inclusive=True)
-        wykaz_dochodów = df.loc[(df['Time']) & (df['Kwota operacji'] > 0)][['Data księgowania', 'Kwota operacji']]
-        wykaz_wydatków = df.loc[(df['Time']) & (df['Kwota operacji'] < 0)][['Data księgowania', 'Kwota operacji']]
-        suma_dochodów = round(df.loc[(df['Time']) & (df['Kwota operacji'] > 0)]['Kwota operacji'].sum(), 2)
-        suma_wydatków = round(df.loc[(df['Time']) & (df['Kwota operacji'] < 0)]['Kwota operacji'].sum(), 2)
-        print(f"Okres od {start_date} do {end_date}:\n{wykaz_dochodów}\nSuma dochodów: {suma_dochodów} zł")
-        print(f"Okres od {start_date} do {end_date}:\n{wykaz_wydatków}\nSuma wydatków: {suma_wydatków} zł")
-        
+        wykaz_dochodow = df.loc[(df['Time']) & (df['Kwota operacji'] > 0)][
+            ['Kwota operacji', 'Data księgowania', 'Kategoria']]
+        wykaz_wydatkow = df.loc[(df['Time']) & (df['Kwota operacji'] < 0)][
+            ['Kwota operacji', 'Data księgowania', 'Kategoria']]
+        suma_dochodow = round(df.loc[(df['Time']) & (df['Kwota operacji'] > 0)]['Kwota operacji'].sum(), 2)
+        suma_wydatkow = round(df.loc[(df['Time']) & (df['Kwota operacji'] < 0)]['Kwota operacji'].sum(), 2)
+        ilosc_transakcji_przychodu = len(df.loc[(df['Time']) & (df['Kwota operacji'] > 0)]['Kwota operacji'])
+        ilosc_transakcji_wydatkow = len(df.loc[(df['Time']) & (df['Kwota operacji'] < 0)]['Kwota operacji'])
+        zysk = round(suma_dochodow + suma_wydatkow, 2)
+        print(f"W okresie od {start_date} do {end_date} przeprowadzono "
+              f"{df.loc[df['Time']].count().iloc[0]} transakcje.\n"
+              f"Suma dochodów: {suma_dochodow} zł ({ilosc_transakcji_przychodu} transakcje)\n"
+              f"Suma wydatków: {suma_wydatkow} zł ({ilosc_transakcji_wydatkow} transakcje)")
+        if zysk > 0:
+            print(f"Zysk wyniósł: {zysk} zł\n")
+        else:
+            print(f"Strata wyniosła: {zysk} zł\n")
+        print(f"Dochody w wybranym okresie to:\n{wykaz_dochodow}\n")
+        print(f"Wydatki w wybranym okresie to:\n{wykaz_wydatkow}\n")
+
     @staticmethod    
     def roczne_info(year):
         Transakcje.przychody_wydatki_roczne(year)
-        Transakcje.liczba_transakcji_rok(year)
-        
+
     @staticmethod
     def miesięczne_info(year, month):
         Transakcje.przychod_wydatki_miesięczne(year, month)
-        Transakcje.liczba_transakcji_miesięcznie(year, month)
-        
+
     @staticmethod
     def okresowe_info(start_date, end_date):
         Transakcje.przychód_wydatki_okres(start_date, end_date)
-        Transakcje.liczba_transakcji_okres(start_date, end_date)
-
+        
+        
 # przykładowe dane              
 Transakcje.roczne_info(2021)
 Transakcje.miesięczne_info(2020, 11)
