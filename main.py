ff0d69d3aeb44dbe54435fb7684d78176d9fa897
GMT-Mati <75912725+GMT-Mati@users.noreply.github.com>
2021-04-16 22:00:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -22,12 +22,30 @@ class AllData:
         self.month = month
         self.start_date = start_date
         self.end_date = end_date
-        
+
+    @staticmethod
+    def month_name(month):
+        return {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June', 7: 'July',
+                8: 'August', 9: 'September', 10: 'October', 11: 'November', 12: 'December'}[month]
+
+    @staticmethod
+    def year_date():
+        df['Year'] = df['Posting Date'].apply(lambda time: time.year)
+
+    @staticmethod
+    def period_date(start_date, end_date):
+        df['Time'] = df['Posting Date'].between(start_date, end_date, inclusive=True)
+
+    @staticmethod
+    def month_date(year):
+        df['Month'] = df['Posting Date'].apply(lambda time: (time.year, time.month))
+
     class Transaction:
-        
+
         @staticmethod
         def annual_tr(year):
-            df['Year'] = df['Posting Date'].apply(lambda time: time.year)
+
+            AllData.year_date()
             income_list = df.loc[(df['Year'] == year) & (df['Amount'] > 0)][['Amount', 'Posting Date', 'Category']]
             expenses_list = df.loc[(df['Year'] == year) & (df['Amount'] < 0)][['Amount', 'Posting Date', 'Category']]
             income_sum = round(df.loc[(df['Year'] == year) & (df['Amount'] > 0)]['Amount'].sum(), 2)
@@ -47,12 +65,9 @@ class AllData:
 
         @staticmethod
         def monthly_tr(year, month):
-            df['Month'] = df['Posting Date'].apply(lambda time: (time.year, time.month))
 
-            def month_name(month):
-                return {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June', 7: 'July',
-                        8: 'August', 9: 'September', 10: 'October', 11: 'November', 12: 'December'}[month]
-
+            AllData.month_date(year)
+            AllData.month_name(month)
             income_list = df.loc[(df['Month'] == (year, month)) & (df['Amount'] > 0)][
                 ['Amount', 'Posting Date', 'Category']]
             expenses_list = df.loc[(df['Month'] == (year, month)) & (df['Amount'] < 0)][
@@ -62,7 +77,7 @@ class AllData:
             income_num_tr = len(df.loc[(df['Month'] == (year, month)) & (df['Amount'] > 0)]['Amount'])
             expenses_num_tr = len(df.loc[(df['Month'] == (year, month)) & (df['Amount'] < 0)]['Amount'])
             profit = round(income_sum + expenses_sum, 2)
-            print(f"In {month_name(month)} {year}, {df[df['Month'] == (year, month)].count().iloc[0]} "
+            print(f"In {AllData.month_name(month)} {year}, {df[df['Month'] == (year, month)].count().iloc[0]} "
                   f"transactions were carried out.\n"
                   f"Total income: {income_sum} USD ({income_num_tr} transactions)"
                   f"\nTotal expenses: {expenses_sum} USD ({expenses_num_tr} transactions)")
@@ -70,12 +85,13 @@ class AllData:
                 print(f"Total profit: {profit} zł\n")
             else:
                 print(f"Total loss: {profit} zł\n")
-            print(f"List of income for {month_name(month)} {year}: \n{income_list.to_string(index=False)}\n")
-            print(f"List of expenses for {month_name(month)} {year}:\n{expenses_list.to_string(index=False)}\n")
+            print(f"List of income for {AllData.month_name(month)} {year}: \n{income_list.to_string(index=False)}\n")
+            print(f"List of expenses for {AllData.month_name(month)} {year}:\n{expenses_list.to_string(index=False)}\n")
 
         @staticmethod
         def periodic_tr(start_date, end_date):
-            df['Time'] = df['Posting Date'].between(start_date, end_date, inclusive=True)
+
+            AllData.period_date(start_date, end_date)
             income = df.loc[(df['Time']) & (df['Amount'] > 0)][
                 ['Amount', 'Posting Date', 'Category']]
             expenses_list = df.loc[(df['Time']) & (df['Amount'] < 0)][
@@ -86,7 +102,8 @@ class AllData:
             expenses_num_tr = len(df.loc[(df['Time']) & (df['Amount'] < 0)]['Amount'])
             profit = round(income_sum + expenses_sum, 2)
             print(
-                f"From {start_date} to {end_date}, {df.loc[df['Time']].count().iloc[0]} transactions were carried out.\n"
+                f"From {start_date} to {end_date}, {df.loc[df['Time']].count().iloc[0]} "
+                f"transactions were carried out.\n"
                 f"Total income: {income_sum} zł ({income_num_tr} transactions)\n"
                 f"Total expenses: {expenses_sum} zł ({expenses_num_tr} transactions)")
             if profit > 0:
@@ -97,10 +114,10 @@ class AllData:
             print(f"List of expenses from {start_date} to {end_date}:\n{expenses_list.to_string(index=False)}\n")
 
     class Salary:
-      
+
         @staticmethod
         def annual(year):
-            df['Year'] = df['Posting Date'].apply(lambda time: time.year)
+            AllData.year_date()
             salary_list = df[(df['Year'] == year) & (df['Title'] == 'Salary')][
                 ['Posting Date', 'Sender/Receiver', 'Amount']]
             salary_value = df[(df['Year'] == year) & (df['Title'] == 'Salary')]['Amount']
@@ -113,25 +130,21 @@ class AllData:
 
         @staticmethod
         def monthly(year, month):
-            df['Month'] = df['Posting Date'].apply(lambda time: (time.year, time.month))
-
-            def month_name(month):
-                return {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June', 7: 'July',
-                        8: 'August', 9: 'September', 10: 'October', 11: 'November', 12: 'December'}[month]
-
+            AllData.month_date(year)
+            AllData.month_name(month)
             salary_list = df[(df['Month'] == (year, month)) & (df['Title'] == 'Salary')][
                 ['Posting Date', 'Sender/Receiver', 'Amount']]
             salary_value = df[(df['Month'] == (year, month)) & (df['Title'] == 'Salary')]['Amount']
             employer = df[(df['Month'] == (year, month)) & (df['Title'] == 'Salary')]['Sender/Receiver']
-            print(f"\nSalary list in {month_name(month)} {year}:\n{salary_list}\n")
-            print(f"Lowest salary in {month_name(month)} {year}:\n{salary_list.min()}\n")
-            print(f"Highest salary in {month_name(month)} {year}:\n{salary_list.max()}\n")
-            print(f'Average salary in {month_name(month)} {year}: {round(salary_value.mean(), 2)} USD\n')
-            print(f"Employer(s) in {month_name(month)} {year}: {', '.join(list(set(employer)))}")
+            print(f"\nSalary list in {AllData.month_name(month)} {year}:\n{salary_list}\n")
+            print(f"Lowest salary in {AllData.month_name(month)} {year}:\n{salary_list.min()}\n")
+            print(f"Highest salary in {AllData.month_name(month)} {year}:\n{salary_list.max()}\n")
+            print(f'Average salary in {AllData.month_name(month)} {year}: {round(salary_value.mean(), 2)} USD\n')
+            print(f"Employer(s) in {AllData.month_name(month)} {year}: {', '.join(list(set(employer)))}")
 
         @staticmethod
         def periodic(start_date, end_date):
-            df['Time'] = df['Posting Date'].between(start_date, end_date, inclusive=True)
+            AllData.period_date(start_date, end_date)
             salary_list = df.loc[(df['Time']) & (df['Title'] == 'Salary')][
                 ['Posting Date', 'Sender/Receiver', 'Amount']]
             salary_value = df.loc[(df['Time']) & (df['Title'] == 'Salary')]['Amount']
@@ -157,7 +170,10 @@ class AllData:
             AllData.Transaction.periodic_tr(start_date, end_date)
 
 
-# FOR EXAMPLE                  
+# FOR EXAMPLE                 
 AllData.Transaction.annual_tr(2021)
-AllData.Salary.monthly(2020, 11)
-AllData.Information.periodic('2021-1-12', '2021-4-12')
+AllData.Transaction.periodic_tr('2020-12-4', '2021-2-18')
+AllData.Salary.annual(2020)
+AllData.Salary.monthly(2021, 4)
+AllData.Salary.periodic('2020-12-4', '2021-2-18')
+AllData.Transaction.monthly_tr(2020, 11)
